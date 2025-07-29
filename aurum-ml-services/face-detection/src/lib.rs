12e2ce3e5fb56e9e2c6d4fe6914f33a0f53e6098
Poom5741 <poom@charoenyost.com>
2025-07-30 02:22:22 +0700

    Remove Rust services from docker-compose and codebase

--- aurum-ml-services/face-detection/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod api;
-pub mod models;
-pub mod processors;
-pub mod utils;
-
-pub use api::{FaceDetectionError, FaceDetectionRequest, FaceDetectionResponse};
-pub use models::{FaceDetection, FaceDetectionModel, ModelError};
-pub use processors::FaceDetector;
-pub use utils::*;
